eb99946acc4bf3d0f7e9bd2b790b9bbe7444d9e8
Marvelbad <badra.aqua@gmail.com>
2025-02-24 20:42:04 +0300

    HomeWork_Two

--- homework-two/src/Main.java
+++ homework-two/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        printStringReverse("Hello World");
+    }
 
+    public static void printStringReverse(String str) {
+        if (str == null || str.isEmpty() || str.trim().isEmpty()) {
+            System.out.println("Wrong string");
+            return;
+        }
+
+        StringBuilder builder = new StringBuilder(str);
+        String result = builder.reverse().toString();
+        System.out.println(result);
     }
 }
