ab158ae5528310cb3d8b2fc1af9d3d39177c25ff
Marvelbad <badra.aqua@gmail.com>
2025-03-06 11:29:30 +0300

    Maven added

--- homework-stream/src/Book.java
+++ homework-stream/src/Book.java
@@ -1,47 +1,24 @@
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Getter
 public class Book {
+    @Setter
     private String author;
+    @Setter
     private String title;
+    @Setter
     private double price;
-    private List<String> reviews;
+    private final List<String> reviews;
 
     public Book(String author, String title, double price) {
         this.author = author;
         this.title = title;
         this.price = price;
         this.reviews = new ArrayList<>();
-    }
-
-    // Геттеры и сеттеры для полей
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public List<String> getReviews() {
-        return reviews;
     }
 
     public void addReview(String review) {
